547dc7d713edd7d742751565c4806afa555aead2
Aaron <aaron@Aarons-Air.lan>
2022-06-17 18:58:23 -0400

    changed key to index

--- client/src/component/Todos.js
+++ client/src/component/Todos.js
@@ -5,8 +5,8 @@ class Todos extends React.Component {
     render() {
         return (
             <div className='container'>
-                {this.props.todos.map((todo) => (
-                    <Todo key={todo.todo_id} todo={todo} />
+                {this.props.todos.map((todo, index) => (
+                    <Todo key={index} todo={todo} />
                 ))}
             </div>
         )
