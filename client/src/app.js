2ed5edea358b1a562406dfc07c3575de5453b697
Aaron <aaron@Aarons-Air.lan>
2022-06-17 17:13:15 -0400

    added loading state and todos

--- client/src/app.js
+++ client/src/app.js
@@ -1,22 +1,30 @@
 import React from "react";
+import Todos from "./component/Todos";
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            todos: null
+            todos: null,
+            loading: true,
+            loadingMessage: 'Please be patient'
         }
     }
 
     componentDidMount() {
         fetch('http://localhost:8000/api/todos')
             .then(result => result.json())
-            .then((data) => this.setState({ todos: data }))
+            .then((data) => this.setState({ todos: data, loading: false }))
     }
 
     render() {
+
+        if (this.state.loading) {
+            return (<h1>{this.state.loadingMessage}</h1>)
+        }
+
         return (
-            <div>Hello World</div>
+            <Todos todos={this.state.todos} />
         )
     }
 }
