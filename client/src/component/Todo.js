d3da5753fd632227fd53c9c5466b50b3b8ca1dbc
Aaron <aaron@Aarons-Air.lan>
2022-06-17 19:00:19 -0400

    added add functionality

--- client/src/component/Todo.js
+++ client/src/component/Todo.js
@@ -3,8 +3,8 @@ import React from "react";
 class Todo extends React.Component {
     render() {
         return (
-            <div id={this.props.todo.todo_id} className="todo">
-                <h1>{this.props.todo.todo}</h1>
+            <div id={this.props.todo.todo_id} className={`todo ${this.props.todo.completed}`}>
+                <h1>{this.props.todo.todo || this.props.todo}</h1>
             </div>
         )
     }
