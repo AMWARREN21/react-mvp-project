9880379443c489a032d444d463ada75e2a37926d
Aaron <aaron@Aarons-Air.lan>
2022-06-17 19:22:49 -0400

    changed completed class

--- client/src/component/Todo.js
+++ client/src/component/Todo.js
@@ -2,9 +2,15 @@ import React from "react";
 
 class Todo extends React.Component {
     render() {
+        let completed = this.props.todo.completed
+
+        if (!this.props.todo.completed) {
+            completed = false
+        }
+
         return (
-            <div id={this.props.todo.todo_id} className={`todo ${this.props.todo.completed}`}>
-                <h1>{this.props.todo.todo || this.props.todo}</h1>
+            <div id={this.props.todo.todo_id} className={`todo ${completed}`}>
+                <h1>{this.props.todo.todo}</h1>
             </div>
         )
     }
