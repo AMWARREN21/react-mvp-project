a66a665153e723e4dc7ea8419fddd8b985465ea3
Aaron <aaron@Aarons-Air.lan>
2022-06-17 19:01:02 -0400

    added form and handles

---
+++ client/src/component/InputTodo.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class InputTodo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ''
+        }
+    }
+
+    render() {
+
+        const handleAddTodo = (e) => {
+            e.preventDefault()
+            this.props.addTodo(this.state.value)
+            this.setState({ value: '' })
+        }
+
+        const handleChange = (e) => {
+            this.setState({ value: e.target.value })
+        }
+
+
+        return (
+            <form onSubmit={handleAddTodo}>
+                <input
+                    value={this.state.value}
+                    type="text"
+                    onChange={handleChange}
+                ></input>
+                <input
+                    type="submit"
+                ></input>
+            </form>
+        )
+    }
+}
+
+export default InputTodo
