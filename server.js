8f337fb763a49ed4a53e4d479ccbf3018c09700b
Aaron <aaron@Aarons-Air.lan>
2022-06-17 15:23:25 -0400

    create get route and created server

---
+++ server.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors'
+import pool from './db/connect.js';
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT || 8000
+
+app.use(express.json());
+app.use(express.static("./client/public"));
+app.use(cors())
+
+app.get('/api/todos', async (req, res) => {
+    try {
+        const client = await pool.connect()
+        const data = await pool.query('SELECT * FROM todos;')
+        res.json(data.rows)
+        client.release()
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT:', PORT)
+})
