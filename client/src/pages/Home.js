32f5525867810a7799c158b0fa6671ed791060dd
Aaron <aaron@Aarons-Air.lan>
2022-06-18 16:08:37 -0400

    added click element

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
+import Clock from "../component/Clock";
 import InputTodo from "../component/InputTodo";
 import Todos from "../component/Todos";
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div className="content-container">
-                <InputTodo addTodo={this.props.addTodo} />
-                <Todos todos={this.props.todos} DeleteTodo={this.props.DeleteTodo} handleEdit={this.props.handleEdit} />
+            <div className="main-container">
+                <div className="content-container">
+                    <InputTodo addTodo={this.props.addTodo} />
+                    <Todos todos={this.props.todos} DeleteTodo={this.props.DeleteTodo} handleEdit={this.props.handleEdit} />
+                </div>
+                <div className="clock-container">
+                    <Clock clock={this.props.clock} />
+                </div>
             </div>
         )
     }
