b0e2067820030fb781ec2dea62ef4681439e614f
Aaron <aaron@Aarons-Air.lan>
2022-06-19 10:48:38 -0400

    consolidated buttons

---
+++ client/src/component/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Button extends React.Component {
+    render() {
+
+        const handleTodo = (e) => {
+            this.props.handleTodo(e)
+        }
+        return (
+            <button className={this.props.class} onClick={handleTodo}>{this.props.class}</button>
+        )
+    }
+}
