bb59eb2ba95db47fb936d486a049a6613b0c8c1f
Aaron <aaron@Aarons-Air.lan>
2022-06-19 16:37:15 -0400

    passing edit function

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,19 +2,20 @@ import React from "react";
 import Clock from "../component/Clock";
 import InputTodo from "../component/InputTodo";
 import Todos from "../component/Todos";
-import EditTodo from "../component/EditTodo";
 
 export default class Home extends React.Component {
     render() {
         return (
             <div className="main-container">
                 <div className="content-container">
-                    <InputTodo addTodo={this.props.addTodo} />
-                    <Todos todos={this.props.todos}
-                        DeleteTodo={this.props.DeleteTodo} />
+                    <InputTodo addTodo={this.props.state.addTodo} />
+                    <Todos
+                        todos={this.props.state.todos}
+                        DeleteTodo={this.props.state.DeleteTodo}
+                        handleEdit={this.props.state.handleEdit} />
                 </div>
                 <div className="clock-container">
-                    <Clock clock={this.props.clock} />
+                    <Clock clock={this.props.state.clock} />
                 </div>
             </div>
         )
