b2c9775e7061fb8f601b09f44f7bdfad8cee07ca
Aaron <aaron@Aarons-Air.lan>
2022-06-19 10:50:51 -0400

    reformatted

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Clock from "../component/Clock";
 import InputTodo from "../component/InputTodo";
 import Todos from "../component/Todos";
+import EditTodo from "../component/EditTodo";
 
 export default class Home extends React.Component {
     render() {
@@ -9,7 +10,8 @@ export default class Home extends React.Component {
             <div className="main-container">
                 <div className="content-container">
                     <InputTodo addTodo={this.props.addTodo} />
-                    <Todos todos={this.props.todos} DeleteTodo={this.props.DeleteTodo} handleEdit={this.props.handleEdit} />
+                    <Todos todos={this.props.todos}
+                        DeleteTodo={this.props.DeleteTodo} />
                 </div>
                 <div className="clock-container">
                     <Clock clock={this.props.clock} />
