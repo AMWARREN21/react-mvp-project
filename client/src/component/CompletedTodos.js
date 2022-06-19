f356af4b5b24e1402cb2f617b7fd27aec9ff2974
Aaron <aaron@Aarons-Air.lan>
2022-06-19 16:41:45 -0400

    filter out needed todos

---
+++ client/src/component/CompletedTodos.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Todo from './Todo'
+
+class CompleteTodos extends React.Component {
+    render() {
+        return (
+            <div className='container'>
+                {this.props.todos.filter(todo => todo.completed !== false)
+                    .map((todo) => (
+                        <Todo
+                            key={todo.todo_id}
+                            todo={todo}
+                            DeleteTodo={this.props.DeleteTodo}
+                            HandleEdit={this.props.handleEdit} />
+                    ))}
+            </div>
+        )
+    }
+}
+
+export default CompleteTodos
