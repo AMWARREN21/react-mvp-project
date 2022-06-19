d17ac04997c5b96d1e73154abf3090dedc8d0c67
Aaron <aaron@Aarons-Air.lan>
2022-06-19 18:44:10 -0400

    heroku ready

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ dotenv.config()
 const PORT = process.env.PORT || 8000
 
 app.use(express.json());
-app.use(express.static("./client/public"));
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 app.use(cors())
 
 app.get('/api/todos', async (req, res) => {
