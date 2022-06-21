684e5a4d0639d4fdda4f37b7f9da7accecfba70a
Aaron <aaron@Aarons-Air.lan>
2022-06-21 12:17:02 -0400

    getting ready for deploy

--- server.js
+++ server.js
@@ -10,21 +10,25 @@ const PORT = process.env.PORT || 8000
 
 app.use(express.json());
 app.use(cors())
-// // Step 1:
+
 app.use(express.static(path.resolve(__dirname, "./client/build")));
-// // Step 2:
+
 app.get("/", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+});
+
+app.get("/home", function (req, res) {
     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-app.get("/home", function (request, res) {
+
+app.get("/complete", function (req, res) {
     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-app.get("/complete", function (request, res) {
+
+app.get("/incomplete", function (req, res) {
     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-app.get("/incomplete", function (request, res) {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+
 
 app.get('/api/todos', async (req, res) => {
     try {
