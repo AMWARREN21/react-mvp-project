ae3a657893e1a1287084bd07e27d9a490c55f552
Aaron <aaron@Aarons-Air.lan>
2022-06-21 11:32:07 -0400

    getting heroku ready

--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,7 @@ dotenv.config()
 dotenv.config()
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL
     // ssl: {
     //     rejectUnauthorized: false
     // }
